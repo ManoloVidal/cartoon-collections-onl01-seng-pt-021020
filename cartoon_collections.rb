570a8b135d8be39a0a565ed0e85f854b7322c902
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-21 19:08:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  
+  cheese_types = ["cheddar", "gouda", "camembert"]
   array.select do |str|
     cheese_types.include?(str)
   end 
