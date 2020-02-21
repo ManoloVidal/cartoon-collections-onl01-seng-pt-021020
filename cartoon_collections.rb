df13e46eed22df795f1a21eeca9b952525bcd25f
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-21 19:09:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#require 'pry'
+def roll_call_dwarves(arr)
+  arr.each_with_index {|name, idx| print "#{idx+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|el| el.capitalize + '!'}
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.select do |str|
+    cheese_types.include?(str)
+  end.first
+  # the array below is here to help
+  
 end
